750fab0e420a2dfbf50261549c9507bc128ae745
SMakaiTakori <makait87@icloud.com>
2021-02-02 13:42:29 -0500

    updated prod_URL

--- apiRoot.js
+++ apiRoot.js
@@ -1,4 +1,4 @@
 const DEV_URL = "http://localhost:3000/";
-const PROD_URL = "https://pinterestcloneapi.herokuapp.com/";
+const PROD_URL = "https://pinterestclone-client.herokuapp.com/";
 export const API_ROOT =
   process.env.NODE_ENV === "development" ? DEV_URL : PROD_URL;
